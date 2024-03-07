d096379dff6e1c6c9893ccad526d5947a8179b00
hiteshrawat13 <hiteshrawat13@gmail.com>
2024-03-07 21:19:59 +0530

    a

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -4,12 +4,10 @@ exports.authenticate = (req, res, next) => {
 
     if (authHeader) {
         const token = authHeader.split(' ')[1];
-
         jwt.verify(token, accessTokenSecret, (err, user) => {
             if (err) {
                 return res.sendStatus(403);
             }
-
             req.user = user;
             next();
         });
